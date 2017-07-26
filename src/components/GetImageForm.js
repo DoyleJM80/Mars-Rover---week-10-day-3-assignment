ce8e38399b37712d8f7092d4ee3769df2e68ec26
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-07-26 13:47:39 -0400

    added components

---
+++ src/components/GetImageForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class GetImageForm extends Component {
+  constructor() {
+    super();
+  };
+  render() {
+    return(
+
+    );
+  };
+}
---
+++ src/components/ImageDisplay.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class ImageDisplay extends Component {
+  constructor() {
+    super();
+  };
+  render() {
+    return(
+
+    );
+  };
+}
